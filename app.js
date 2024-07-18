c175bbfe4e1681766f6ca2193708295838f01731
Manoj Vaddi <141347229+manojvaddi97@users.noreply.github.com>
2024-07-18 17:09:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }))
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const secret = (process.env.SECRET || 'thisshouldbeabettersecret!').replace(/\s+/g, '');
 
 const store = new MongoDBStore({
     url: dbUrl,
